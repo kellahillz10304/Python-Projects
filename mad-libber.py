713143290f92d44a50eef2c2c99af9db8efedc80
Chris <cjk5029@gmail.com>
2022-09-26 09:40:38 -0400

    Mad-Libber Python Project

---
+++ mad-libber.py
@@ -0,0 +1,6 @@
+name = str(input( "Enter a name: " ))
+place = str(input( "Enter a place: " ))
+verb = str(input( "Enter a verb: " ))
+print( "Old Mc-", name, "had a", place, "E-I-E-I-O And on that airport he liked to\n",
+       verb, "E-I-E-I-O With a", verb, verb, "here, and a", verb, verb, "there, everywhere a", verb, verb )
+           
